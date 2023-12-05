2462e9eee708faebd7fd68469918a17be2ab570d
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2023-12-05 21:04:28 +0100

    final

---
+++ 10_WHILE/script.js
@@ -0,0 +1,41 @@
+// let i=1;
+// while (i<=5) {
+// console.log(i);
+// i++;
+// }
+// console.log("Kraj petlje");
+// console.log(i);
+
+// 1. Zadatak
+
+// let x = 1;
+// while (x<=20) { 
+// console.log(x);
+// console.log(x++);
+
+// }
+
+// 2. Zadatak
+// let y = 20;
+// while (y <=20 && y>0 ) { 
+//     console.log(y);
+//     y--;
+// }
+// console.log( "Kraj" );
+//3. Zadatak
+// i=1;
+// while (i <= 20) 
+// {
+//     if (i % 2 == 0) {
+//         console.log(i);
+// }
+//     i++;
+// }
+// console.log("kraj");
+//4.Zadatak
+let i=1
+while (i >= 6){
+if (i%2 ==0){
+    document.write(<p style="color:red"> lorem ipsum dolores </p>)
+}
+}
