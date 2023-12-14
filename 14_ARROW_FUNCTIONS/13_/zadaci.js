96f19393d6e996fd41b9ed5b39f803377d1225e7
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2023-12-14 20:31:23 +0100

    arrow

--- 14_ARROW_FUNCTIONS/13_/zadaci.js
+++ 14_ARROW_FUNCTIONS/13_/zadaci.js
@@ -8,13 +8,65 @@ function neparan(n) {
     }
 }
 
-let neparan = (n) => {
-    let x=false;
+// let neparan = (n) => {
+//     let x=false;
 
-    if (n%2 == 0) {
+//     if (n%2 == 0) {
     
-        x=true;
+//         x=true;
+//     }
+    
+//         return x;
+// } 
+
+// //* Maks2 
+let maks2 = (a,b) => {
+    let greaterNumber=0;
+(a<b) ? greaterNumber+= b : greaterNumber +=a;
+    return greaterNumber;
+}
+
+//* Maks4
+
+let maks4 = (a,b,c,d) => {
+    let greaterNumber=a;
+    if (greaterNumber < b) {
+        greaterNumber =b;
     }
-    
-        return x;
+    if (greaterNumber < c) {
+        greaterNumber =c;
+    }
+    if (greaterNumber < d) {
+        greaterNumber =d;
+    } return greaterNumber;
+}
+
+// let maks4 = (a,b,c,d) => {
+//     let greaterNumber=a;
+//     (greaterNumber < b) ?
+//     greaterNumber =b :
+//     (greaterNumber <c) ? 
+//     greaterNumber = c :
+//     (greaterNumber <d) ?
+//     greaterNumber = d :
+//     greaterNumber = a;
+//     return greaterNumber;
+// }
+console.log(maks4(1,15,2,22));
+console.log(maks2(15,22));
+
+// const getPic = (adress) => {
+//     let pic;
+//     pic += document.write(`<img src="adress">`)
+//     return pic;
+// }
+// let div = document.getElementById('divko');
+
+let getPic = address => {
+    let img = document.createElement('img'); 
+    img.src = address;
 } 
+let div = document.getElementById ('divko');
+div.innerHTML += getPic()
+
+
