2c063652c3c3f598cbb71a2d3e3799d1b44fe6ac
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2023-12-14 19:16:28 +0100

    arrow

---
+++ 14_ARROW_FUNCTIONS/13_/script.js
@@ -0,0 +1,8 @@
+let suma = (a,b) => {
+    return a+b;
+}
+
+let echo = (s1,s2) => {
+let result = s1 + "" +s2;
+return result;
+}
---
+++ 14_ARROW_FUNCTIONS/13_/zadaci.js
@@ -0,0 +1,20 @@
+//* Zadatak 3
+
+function neparan(n) {
+    if (n%2 == 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let neparan = (n) => {
+    let x=false;
+
+    if (n%2 == 0) {
+    
+        x=true;
+    }
+    
+        return x;
+} 
