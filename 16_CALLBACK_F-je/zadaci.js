0435d78b3080259919bacf1a11ed0123ed2541f6
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2023-12-21 19:46:47 +0100

    call

--- 16_CALLBACK_F-je/zadaci.js
+++ 16_CALLBACK_F-je/zadaci.js
@@ -29,15 +29,13 @@ console.log(proizvod(b));
 //* Zadatak 3 
 let srVr= niz => {
     let suma=0;
-    let brojac=0;
     niz.forEach(el => {
         suma += el;
-        brojac++;
-    }); return suma/brojac;
+    }); return suma/niz.length;
 }
 console.log(srVr(a));
 
-//* Zadatak 5*
+//* Zadatak 6
 let minVr = niz => {
     let min = niz[0];
     niz.forEach(elem => {
@@ -45,3 +43,58 @@ let minVr = niz => {
     }); return min;
 }
 console.log(minVr(a));
+console.log(minVr(b));
+
+
+//* Zadatak 7
+let iMaxEl = niz => {
+    let max = niz[0];
+    let maxIndex =0;
+    niz.forEach((elem,index) => {
+         if (elem > max) {
+            max = elem;
+            maxIndex = index;
+        }
+    }); return maxIndex;
+}
+console.log(iMaxEl(a));
+console.log(iMaxEl(b));
+
+//* Zadatak 8
+
+let iMinEl = niz => {
+    let minEl=0;
+    let min= niz[0];
+    niz.forEach((el,index)=>{
+        if (el < min){
+            min+=el;
+            minEl=index;
+        }
+    }); return minEl;
+}
+//* Zadatak 9
+
+let brElSrVr= niz => {
+    let suma=0;
+    let srVr =0;
+    let brEl = 0;
+    niz.forEach(el => {
+        suma += el;
+        srVr = suma/niz.length;
+        (el > srVr) ?  brEl++ : false
+    }); return brEl;
+}
+// console.log(brElSrVr(a));
+// console.log(brElSrVr(b));
+
+//* Zadatak 19
+
+let duzinaSvihStringova = niz => {
+    niz.forEach(el => {
+        console.log(el.length);
+    })
+}
+
+let imena = ["Stefan","Vladislav","Nikola","Nevena","Maja"];
+duzinaSvihStringova(imena);
+
