7bc58b814150c4db4760a36e069f2924d4fd6391
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2023-12-25 18:42:12 +0100

    nizovi objekata

---
+++ 18_Nizovi-Objekata/users.js
@@ -0,0 +1,28 @@
+let blog1 = {
+  title: " IF naredba grananja !",
+  likes: 50,
+  dislikes: 21,
+};
+let blog2 = {
+  title: " While petlja ",
+  likes: 25,
+  dislikes: 36,
+};
+let blog3 = {
+  title: " For petlja ",
+  likes: 100,
+  dislikes: 12,
+};
+
+let user1 = {
+  username: "BakiP",
+  age: 26,
+  blogs: [blog1, blog3],
+};
+
+let user2 = {
+  username: "BakiM",
+  age: 20,
+  blogs: [blog2],
+};
+///
