0468e09039ee26af52b7f188330dd0a69c0dafda
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2023-12-26 21:02:22 +0100

    DOM

--- 19_DOM/script.js
+++ 19_DOM/script.js
@@ -1,4 +1,3 @@
-let e1 = document.getElementById("");
 let el2 = document.getElementsByClassName("par");
 
 for (let i = 0; i < el2.length; i++) {
@@ -12,19 +11,33 @@ niz.forEach((element) => {
 
 let t1 = document.querySelector("#p2");
 let prviParagraf = document.querySelector("p");
-let prviDivError = document.querySelector("div.error");
+let svakiDivError = document.querySelectorAll("div.error");
 let tabela = document.getElementsByClassName("tabela");
 let red = document.getElementsByTagName("tr");
 let poslednjiRed = red[red.length - 1];
 let sveSlike = document.querySelectorAll("img");
 let sviLinkovi = document.querySelectorAll("a");
 let prviLink = document.querySelector("a");
+let sviP = document.querySelectorAll("p");
 
 console.log(sveSlike);
 console.log(sviLinkovi);
-console.log(prviDivError);
+console.log(svakiDivError);
 console.log(prviParagraf);
 console.log(poslednjiRed);
 
 prviLink.innerHTML =
   "<span style=' color: red; font-weight: bold; '>Novi tekst</span> prvog linka";
+
+//*
+sviP.innerHTML += "VAZNO!!!";
+//*
+svakiDivError.forEach((element) => {
+  element.innerHTML = "<h1>Greska!</>";
+});
+
+//*
+for (i = 0; i < sviP.length; i++) {
+  sviP[i].innerHTML += i ** 2;
+}
+//*
