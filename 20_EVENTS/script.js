14fc425025e378d1ece5673d00bdda89fa622a48
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2023-12-29 19:24:57 +0100

    vezbanja DOM and EVENTS

--- 20_EVENTS/script.js
+++ 20_EVENTS/script.js
@@ -1,26 +1,102 @@
-let btnHello = document.querySelector(".btn");
-let btnBroj = document.querySelector(".broji");
-let paragraf = document.querySelector("#ispis");
-let btnMinus = document.querySelector(".btnMinus");
-let btnPlus = document.querySelector(".btnPlus");
-let prikaz = document.querySelector(".br");
+// let br = 0;
 
-let br = 0;
+// btnMinus.addEventListener("click", () => {
+//   if (br == 0) {
+//     br = 0;
+//     prikaz.innerHTML = br;
+//   } else {
+//     br--;
+//     prikaz.innerHTML = br;
+//   }
+// });
+// btnPlus.addEventListener("click", () => {
+//   br++;
+//   prikaz.innerHTML = br;
+// });
+//* Zadatak 5
+// let btnIspisiTekst = document.querySelector(".ispisiTekst");
+// let pIspisTeksta = document.querySelector(".ispisTeksta");
+// let inputTekst = document.querySelector(".tekst");
 
-btnMinus.addEventListener("click", () => {
-  br--;
-  prikaz.innerHTML = br;
+// btnIspisiTekst.addEventListener("click", () => {
+//   let preuzmiTekst = inputTekst.value;
+//   pIspisTeksta.innerHTML = `Zdravo ${preuzmiTekst}`;
+// });
+
+//* Zadatak 6 ( Dopuna 5. zadatka )
+
+//? Dopuniti 5. zadatak dodavanjem radio button polja gde korisnik može odabrati svoj pol (muški, ženski ili neopredeljen).Nakon duplog klika na dugme, u konzoli ispisati pol koji je osoba odabrala.
+
+// let radioMuski = document.querySelector(".M");
+// let radioZenski = document.querySelector(".Z");
+// let radioNeopredeljen = document.querySelector(".N");
+
+// btnIspisiTekst.addEventListener("dblclick", () => {
+//* Prvi nacin
+// if (radioMuski.checked) {
+//   console.log("Osoba je muskog pola.");
+// } else if (radioZenski.checked) {
+//   console.log("Osoba je zenskog pola.");
+// } else if (radioNeopredeljen.checked) {
+//   console.log("Osoba se nije opredelila u vezi pola.");
+// } else {
+//   console.log("Nije selektovana nijedna opcija!");
+// }
+//* Drugi nacin
+// let checkedPol = document.querySelector("input[name='pol']:checked");
+// console.log(checkedPol.value);
+// if (checkedPol.value == m) {
+//   console.log("Osoba je muskog pola.");
+// } else if (checkedPol.value == z) {
+//   console.log("Osoba je zenskog pola");
+// } else if (checkedPol.value == n) {
+//   console.log("Osoba je neodredjenog pola");
+// } else {
+//   console.log("Nista nije selektovano");
+// }
+//* Treci nacin
+//   let polRadio = document.getElementsByName("pol");
+//   console.log(polRadio);
+//   let polRadioInput = document.querySelector("input[name='pol']");
+//   polRadioInput.forEach((rb) => {
+//     if (rb == checked) {
+//       console.log();
+//     }
+//   });
+// });
+//* 7. Zadatak
+let kvadriraj = document.querySelector(".btn-kvadriraj");
+let prepolovi = document.querySelector(".btn-prepolovi");
+let calcP = document.querySelector(".btn-povrsina");
+
+let unosKvadriraj = document.querySelector(".input-kvadriraj");
+let unosPrepolovi = document.querySelector(".input-prepolovi");
+let unosPovrsina = document.querySelector(".input-povrsina");
+
+let ispisKvadrata = document.querySelector(".ispisKvadrata");
+let ispisPrepolovi = document.querySelector(".ispisPrepolovi");
+let ispisPovrsina = document.querySelector(".ispisPovrsina");
+
+let br1;
+
+kvadriraj.addEventListener("click", (event) => {
+  event.preventDefault();
+  br1 = unosKvadriraj.value;
+  ispisKvadrata.innerHTML = br1 ** 2;
 });
-btnPlus.addEventListener("click", () => {
-  br++;
-  prikaz.innerHTML = br;
+
+let br2;
+
+prepolovi.addEventListener("click", (event) => {
+  event.preventDefault();
+  br2 = unosPrepolovi.value;
+  ispisPrepolovi.innerHTML = br2 / 2;
 });
 
-let btnIspisiTekst = document.querySelector(".ispisiTekst");
-let pIspisTeksta = document.querySelector(".ispisTeksta");
-let inputTekst = document.querySelector(".tekst");
+let R;
 
-btnIspisiTekst.addEventListener("click", () => {
-  let preuzmiTekst = inputTekst.value;
-  pIspisTeksta.innerHTML = `Zdravo ${preuzmiTekst}`;
+calcP.addEventListener("click", (event) => {
+  event.preventDefault();
+  R = unosPovrsina.value;
+  ispisPovrsina.innerHTML = R ** 2 * 3.14;
 });
