14a9bdfc7de9af63c41ded57713cd7bb9f10f3ce
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2024-01-23 18:27:03 +0100

    asinhrone niti

--- PROMISES_VEZBANJA/script.js
+++ PROMISES_VEZBANJA/script.js
@@ -18,6 +18,75 @@ form.addEventListener("submit", (e)=> {
 })
 
 
+function dohvatiZalihe() {
+
+    let request = new XMLHttpRequest();
+
+    request.addEventListener("readystatechange", function() {
+        
+        if(request.readyState === 4 && request.status === 200) {
+            // sve ok, obradjujemo zahtev
+            
+            let data = JSON.parse(request.responseText);
+            resolve(data); // callback funkcija sa kojom radimo nesto
+
+        } else if (request.readyState === 4) {
+        // desila se neka greska
+            reject("Desila se greska")
+        
+        }
+})
+request.open('GET', "JSON/stock.json");
+request.send();
+}
+
+function dohvatiTezine() {
+
+    let request = new XMLHttpRequest();
+
+    request.addEventListener("readystatechange", function() {
+        
+        if(request.readyState === 4 && request.status === 200) {
+            // sve ok, obradjujemo zahtev
+            
+            let data = JSON.parse(request.responseText);
+            resolve(data); // callback funkcija sa kojom radimo nesto
+
+        } else if (request.readyState === 4) {
+        // desila se neka greska
+            reject("Desila se greska")
+        
+        }
+})
+request.open('GET', "JSON/weights.json");
+request.send();
+}
+
+function dohvatiCene() {
+
+    let request = new XMLHttpRequest();
+
+    request.addEventListener("readystatechange", function() {
+        
+        if(request.readyState === 4 && request.status === 200) {
+            // sve ok, obradjujemo zahtev
+            
+            let data = JSON.parse(request.responseText);
+            resolve(data); // callback funkcija sa kojom radimo nesto
+
+        } else if (request.readyState === 4) {
+        // desila se neka greska
+            reject("Desila se greska")
+        
+        }
+})
+request.open('GET', "JSON/prices.json");
+request.send();
+}
+
+
+
+
 function getItems (resource,resolve,reject) {
 
     let request = new XMLHttpRequest();
@@ -43,3 +112,6 @@ function getItems (resource,resolve,reject) {
 
 }
 
+dohvatiZalihe();
+dohvatiTezine();
+dohvatiCene();
