4b7177a4b2227c02773268a1479439eb81b5f937
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2024-01-23 17:57:51 +0100

    asinhroni poziv

---
+++ PROMISES_VEZBANJA/script.js
@@ -0,0 +1,45 @@
+const form = document.querySelector('#order');
+const capacity = document.querySelector('#capacity');
+const div = document.querySelector('#result')
+
+/*!  Odrediti artikle koji vise nisu na stanju */
+/*! Odrediti ukupnu tezinu svih artikala*/
+/*! Ako je ta tezina manja od kapaciteta kamiona, onda:
+3.1 Odrediti i ispisati ukupnu cenu svih tih artikala
+3.2 Inacem ispisati poruku da kamion nema trazeni kapacitet 
+*/ 
+
+
+form.addEventListener("submit", (e)=> {
+
+    e.preventDefault();
+    div.innerHTML = "Submitovana forma"
+
+})
+
+
+function getItems (resource,resolve,reject) {
+
+    let request = new XMLHttpRequest();
+
+    request.addEventListener("readystatechange", function() {
+        
+        if(request.readyState === 4 && request.status === 200) {
+            // sve ok, obradjujemo zahtev
+            
+            let data = JSON.parse(request.responseText);
+            resolve(data); // callback funkcija sa kojom radimo nesto
+
+        } else if (request.readyState === 4) {
+        // desila se neka greska
+            reject("Desila se greska")
+        
+        }
+
+    })
+
+    request.open('GET', resource);
+    request.send();
+
+}
+
